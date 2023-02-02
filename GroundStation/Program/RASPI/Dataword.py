7556c007af779ed4cd8ee304b327e8764dc7ba5a
LukyNtheRaCoon <nas.luky@seznam.cz>
2023-02-02 09:33:05 +0100

    Lora_update

---
+++ GroundStation/Program/RASPI/Dataword.py
@@ -0,0 +1,38 @@
+import time
+
+adsbmeread = ("")
+gpsread = ("")
+
+def ADSBMEread():
+    global adsbmeread
+    
+    with open("/home/pi/Desktop/adsbme.txt", "r") as file1:
+        if file1.readable():
+            adsbmeread = file1.read()
+    print(adsbmeread)
+
+def GPSread():
+    global gpsread
+    
+    with open("/home/pi/Desktop/gps.txt", "r") as file2:
+        if file2.readable():
+            gpsread = file2.read()
+    print(gpsread)
+
+def DataWord():
+    dataword = ("")
+    dataword += (adsbmeread)
+    dataword += (gpsread)
+    #dataword += (loraread)
+
+    with open("/home/pi/Desktop/Data.txt", "a") as file3:
+        file3.write("\n%s" % dataword)
+        
+
+    print(dataword)
+    
+while True:
+    ADSBMEread()
+    GPSread()
+    DataWord()
+    time.sleep(1)
--- GroundStation/Program/RASPI/LORA_transmit.py
+++ GroundStation/Program/RASPI/LORA_transmit.py
@@ -11,20 +11,25 @@ spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 433.0)
 rfm9x.tx_power = 23
 prev_packet = None                              #Reset main storing variable
+packet_text = None
+
+def doNothing():
+    nothing = True
 
 def LoraReceive():                              #Basic lora function
-    
+    global packet_text
     global prev_packet                          #Definition of variables
     packet = None                               #Variable for storing packets (set to NONE every loop)
 
     packet = rfm9x.receive(with_header=True)    #Listening for packets, Header - first 4 letters of message
     if packet is None:
-        print('- Waiting for PKT -')
+        #print('- Waiting for PKT -')
+        doNothing()
     else:
         prev_packet = packet                    #Storing the received packet for further using
         #print(prev_packet)
         packet_text = str(prev_packet, "utf-8") #Decoding packet to an UTF-8 symbol package
-        print('RX: ')
+        #print('RX: ')
         print(packet_text)                      #Printing decoded text
         #-------------------
         #print("Received (raw header):", [hex(x) for x in packet[0:4]])
@@ -33,6 +38,22 @@ def LoraReceive():                              #Basic lora function
         time.sleep(0.1)
     time.sleep(0.1)
 
+def DataWord():
+    global packet_text
+
+    dataword = ("")
+    dataword += ("%s" % packet_text)
+
+    print (dataword)
+
+    with open("/home/pi/Desktop/lora.txt", "w") as file:
+        if file.writable():
+            file.write(dataword)
+    
+
 
 while True:
     LoraReceive()
+    DataWord()
+
+    time.sleep(0.1)
