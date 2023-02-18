3d67257adb0e3fee1ea9744b6d8555afdd2aa62e
Haismd03 <haisman.david@seznam.cz>
2023-02-18 22:35:03 +0100

    Created control task

--- CanSat/Program/Core/src/main.cpp
+++ CanSat/Program/Core/src/main.cpp
@@ -66,6 +66,8 @@ void setup(void) {
   xTaskCreate(loraSend, "Lora Send Task", 4096, NULL, 10, &loraSend_handle);
   xTaskCreate(getData, "Get Data Task", 4096, NULL, 5, NULL);
   xTaskCreate(runNeo, "Run Neo Pixels", 2048, NULL, 3, &runNeo_handle);
+  xTaskCreate(controlTask, "Control Task", 8192, NULL, 1, NULL);
+
   xTaskCreate(isrHandleDioRise, "ISR DIO Rise", 2048, NULL, 15, &isrHandleDioRise_handle);
 
   /*xTaskCreate(
