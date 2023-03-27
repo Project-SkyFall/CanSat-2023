e4a5633c4ca8fd39aeaa9fca66aa3a91e7460355
brzdvt <113098495+brzdvt@users.noreply.github.com>
2023-03-27 22:33:44 +0200

    Main update

--- Visualization/main.py
+++ Visualization/main.py
@@ -37,8 +37,8 @@ phase = 1
 black = (0,0,0)
 
 # LOAD FONT
-Font = pg.font.Font(r"font/myriad-pro/MyriadPro-Light.otf", 14)
-Font_RT = pg.font.Font(r"font/myriad-pro/MyriadPro-Light.otf", 11)
+Font = pg.font.Font(r"font\myriad-pro\MyriadPro-Light.otf", 14)
+Font_RT = pg.font.Font(r"font\myriad-pro\MyriadPro-Light.otf", 11)
 
 # SETUP PHASE I
 # COORDINATES FOR "dead or alive" AND "up and down"
@@ -94,15 +94,15 @@ heightMin = 0
 heightMax = 1000
 
 # LOAD IMAGES
-background = pg.image.load(r"GUI_grafika/GG_background2.png")
-RucickaTemp = pg.image.load(r"GUI_grafika/RucickaTemp2.png")
-Alive = pg.image.load(r"GUI_grafika/Active3.png")
-Down = pg.image.load(r"GUI_grafika/Down3.png")
-Dead = pg.image.load(r"GUI_grafika/Inactive3.png")
-RucickaHeight = pg.image.load(r"GUI_grafika/RucickaHeight3.png")
-RucickaCO2O2 = pg.image.load(r"GUI_grafika/RucickaO2CO22.png")
-RucickaPress = pg.image.load(r"GUI_grafika/RucickaPress2.png")
-Up = pg.image.load(r"GUI_grafika/Up3.png")
+background = pg.image.load(r"GUI_grafika\GG_background2.png")
+RucickaTemp = pg.image.load(r"GUI_grafika\RucickaTemp2.png")
+Alive = pg.image.load(r"GUI_grafika\Active3.png")
+Down = pg.image.load(r"GUI_grafika\Down3.png")
+Dead = pg.image.load(r"GUI_grafika\Inactive3.png")
+RucickaHeight = pg.image.load(r"GUI_grafika\RucickaHeight3.png")
+RucickaCO2O2 = pg.image.load(r"GUI_grafika\RucickaO2CO22.png")
+RucickaPress = pg.image.load(r"GUI_grafika\RucickaPress2.png")
+Up = pg.image.load(r"GUI_grafika\Up3.png")
 
 # GET RECTS
 background_Rect = background.get_rect()
@@ -144,145 +144,179 @@ press_pre = None
 temp_pre = None
 
 # TEST
-#j = 0
-#dolu0 = False
-#dolu1 = False
-#data = []
-#for i in range(20):
-#    data.append(0)
+test = False
+if test:
+    j = 0
+    dolu0 = False
+    dolu1 = False
+    data = []
+    for i in range(53):
+        data.append(0)
 
 # SET PRE TEXT VALUE
 line_pre = None
 
 # PHASE I
 def phase1():
-    # TODO podminka, že mám nová data
+    # COMMUNICATION AND PARSE DATA
     global line_pre
     global recieve
 
-    try:
-        fh = open(path, "r")
-        line = fh.readline()
-        fh.close()
-    except:
-        print("chyba cteni souboru")
-        return
+    if test:
+        global data
 
-    if (line_pre != None) and (line == line_pre):
-        #print("data shodna s predchozimi")
-        return
-
-    data_str = line.strip().split(";")
-    line_pre = line
-
-    data = []
-    text = False
-    recieve = ""
-    for dat_str in data_str:
+    if not test:
         try:
-            dat = float(dat_str)
-            data.append(dat)
+            fh = open(path, "r")
+            line = fh.readline()
+            fh.close()
         except:
-            recieve += dat_str
+            print("chyba cteni souboru")
+            return
 
-    print(data)
-    print(recieve)
-        
-#    global data
+        if (line_pre != None) and (line == line_pre):
+            print("data shodna s predchozimi")
+            return
+
+        data_str = line.strip().split(";")
+        line_pre = line
+
+        data = []
+        text = False
+        recieve = ""
+        for dat_str in data_str:
+            try:
+                dat = float(dat_str)
+                data.append(dat)
+            except:
+                recieve += dat_str
+
+        print(data)
+        print(recieve)
+
     
     global press_pre
     global temp_pre
     
     # TEST
-#    global dolu0
-#    global dolu1
-#    global j
-    
-#    for i in range(len(data)):
-#        data[i] += 0.1
-#    if dolu0:
-#        data[0] -= 0.2
-#    if data[0] > 120:
-#        dolu0 = True
-#    if data[0] < 60:
-#        dolu0 = False
-#    if dolu1:
-#        data[1] -= 0.2
-#    if data[1] > 65:
-#        dolu1 = True
-#    if data[1] < -10:
-#        dolu1 = False
-#    data[2] += 0.9
-#    if data[2] > 1100:
-#        data[2] = 300
-#    data[3] -= 0.09
-#    if data[3] > 22:
-#        data[3] = 18
-#    if data[4] > 1100:
-#        data[4] = -100
-#    
-#    if data[5] > 100:
-#        data[5] = 0
+    if test:
+        
+        global dolu0
+        global dolu1
+        global j
+        
+        for i in range(len(data)):
+            data[i] += 0.1
+        if dolu0:
+            data[0] -= 0.2
+        if data[0] > 120:
+            dolu0 = True
+        if data[0] < 60:
+            dolu0 = False
+        if dolu1:
+            data[1] -= 0.2
+        if data[1] > 65:
+            dolu1 = True
+        if data[1] < -10:
+            dolu1 = False
+        data[2] += 0.9
+        if data[2] > 1100:
+            data[2] = 300
+        data[3] -= 0.09
+        if data[3] > 22:
+            data[3] = 18
+        if data[4] > 1100:
+            data[4] = -100
+        
+        if data[5] > 100:
+             data[5] = 0
+        j += 1
+        if j%1000 <500:
+            for i in range(8):
+                data[6+i] = 1
+        else:
+            for i in range(8):
+                data[6+i] = 0
+        data[22] -= 0.099
+        data[23] -= 0.099
 
-#    j += 1
-#    if j%1000 <500:
-#        for i in range(8):
-#            data[6+i] = 1
-#    else:
-#        for i in range(8):
-#            data[6+i] = 0
+    # ASING DATA
+    batteryGS = data[0]    
+    tempGS = data[1]
+    humidityGS = data[2]
+    pressGS = data[3]
+    altGS = data[4]
+    timeGS = data[5]
+    latGS = data[6]
+    longGS = data[7]
+    rangeCan = data[8]
 
-    # TODO PARSE AND ASING DATA
-    try:
-        press = data[4]
-    
-        temp = data[3]
+    dORa0 = data[9]
+    dORa1 = data[10]
+    dORa2 = data[11]
+    dORa3 = data[12]
+    dORa4 = data[13]
+    dORa5 = data[14]
+    dORa7 = data[15]
 
-        co2 = data[8]
+    lightintensity = data[16]
+    temp = data[17]
+    press = data[18]
+    humidity = data[19]
+    battery = data[20]
+    co2 = data[21]
 
-        o2 = data[21]
+    dORa6 = data[22]
+    lat = data[23]
+    long = data[24]
+    height = data[25]
+    speed = data[26]
+    time = data[27]
+    sattelitesNum = data[28]
 
-        height = data[12]
+    roll = data[29]
+    pitch = data[30]
+    yaw = data[31]
 
-        speed = data[13]
+    o2 = data[32]
 
-        dORa0 = data[2]
-        dORa1 = data[16]
-        dORa2 = data[20]
-        dORa3 = data[7]
-        dORa4 = data[1]
-        dORa5 = data[26]
-        dORa6 = data[9]
-        dORa7 = data[44]
+    asx0 = data[33]
+    asx1 = data[34]
+    asx2 = data[35]
+    asx3 = data[36]
+    asx4 = data[37]
+    asx5 = data[38]
+    asx6 = data[39]
+    asx7 = data[40]
+    asx8 = data[41]
+    asx9 = data[42]
+    asx10 = data[43]
+    asx11 = data[44]
+    asx12 = data[45]
+    asx13 = data[46]
+    asx14 = data[47]
+    asx15 = data[48]
+    asx16 = data[49]
+    asx17 = data[50]
 
-        humidity = data[5]
+    transfer = data[51]
+    currentAntena = data[52]
 
-        transfer = 1000 / data[46]
-
-        battery = data[6]
-
-        roll = data[17]
-        pitch = data[18]
-        yaw = data[19]
-    
-    except:
-        screen.fill(black)
-        screen.blit(background, background_Rect)
-        Text_Recieve = Font.render(recieve, True, black)
-        Text_Recieve_Rect = Text_Recieve.get_rect(center=(128,438))
-        screen.blit(Text_Recieve, Text_Recieve_Rect)
-        return
-    
-    
-#    roll -=0.05
-#    pitch -=0.05
-#    yaw -=0.05
 
     # CALCULATE LAND IN
     if speed != 0:
         landIn = height/speed
     else:
         landIn = 0
+
+    # CALCULATE LAT AND LONG
+    degN = int(lat)
+    minN = int((lat-degN)*60)
+    vteN = round(((lat-degN)*60-minN)*60, 1)
+
+    degE = int(long)
+    minE = int((long-degE)*60)
+    vteE = round(((long-degE)*60-minE)*60, 1)
 
     # SETUP NEW FRAME
     screen.fill(black)
@@ -377,6 +411,12 @@ def phase1():
         
 
     # TEXTS
+    Text_Name = Font.render(r"X-Sat", True, black)
+    Text_Name_Rect = Text_Name.get_rect(center=(129,77))
+
+    Text_BatteryGS = Font.render(str(round(batteryGS)), True, black)
+    Text_BatteryGS_Rect = Text_BatteryGS.get_rect(center=(400,32))
+
     Text_Press = Font.render(str(round(press, 1)), True, black)
     Text_Press_Rect = Text_Press.get_rect(center=(306,142))
 
@@ -389,22 +429,25 @@ def phase1():
     Text_CO2 = Font.render(str(round(co2)), True, black)
     Text_CO2_Rect = Text_CO2.get_rect(center=(306,274))
 
+    Text_Range = Font.render(str(round(rangeCan)), True, black)
+    Text_Range_Rect = Text_Range.get_rect(center=(658,346))
+
     Text_Height = Font.render(str(round(height)), True, black)
-    Text_Height_Rect = Text_Height.get_rect(center=(658,350))
+    Text_Height_Rect = Text_Height.get_rect(center=(658,371))
 
     Text_Speed = Font.render(str(round(speed)), True, black)
-    Text_Speed_Rect = Text_Speed.get_rect(center=(654,369))
+    Text_Speed_Rect = Text_Speed.get_rect(center=(654,410))
 
     Text_LandIn = Font.render(str(round(landIn, 1)), True, black)
-    Text_LandIn_Rect = Text_LandIn.get_rect(center=(657,388))
+    Text_LandIn_Rect = Text_LandIn.get_rect(center=(657,430))
 
     Text_RT = Font_RT.render(r"00:00:00    00.00.0000", True, black)
-    Text_RT_Rect = Text_RT.get_rect(center=(650, 409))
+    Text_RT_Rect = Text_RT.get_rect(center=(650, 451))
 
     Text_Humidity = Font.render(str(round(humidity, 1)), True, black)
     Text_Humidity_Rect = Text_Humidity.get_rect(center=(399,200))
     
-    Text_Transfer = Font.render(str(round(transfer, 1)), True, black)
+    Text_Transfer = Font.render(str(round(transfer)), True, black)
     Text_Transfer_Rect = Text_Transfer.get_rect(center=(150,293))
     
     Text_Battery = Font.render(str(round(battery, 1)), True, black)
@@ -412,16 +455,46 @@ def phase1():
 
     Text_Sent = Font.render(sent, True, black)
     Text_Sent_Rect = Text_Sent.get_rect(center=(128,374))
-    
+
     Text_Recieve = Font.render(recieve, True, black)
     Text_Recieve_Rect = Text_Recieve.get_rect(center=(128,438))
-    
+
+    Text_Roll = Font_RT.render(str(round(roll)), True, black)
+    Text_Roll_Rect = Text_Roll.get_rect(center=(117, 255))
+
+    Text_Pitch = Font_RT.render(str(round(pitch)), True, black)
+    Text_Pitch_Rect = Text_Pitch.get_rect(center=(149, 255))
+
+    Text_Yaw = Font_RT.render(str(round(yaw)), True, black)
+    Text_Yaw_Rect = Text_Yaw.get_rect(center=(181, 255))
+
+    Text_degN = Font_RT.render(str(round(degN)), True, black)
+    Text_degN_Rect = Text_degN.get_rect(center=(641, 255))
+
+    Text_minN = Font_RT.render(str(round(minN)), True, black)
+    Text_minN_Rect = Text_minN.get_rect(center=(658, 255))
+
+    Text_vteN = Font_RT.render(str(round(vteN, 1)), True, black)
+    Text_vteN_Rect = Text_vteN.get_rect(center=(677, 255))
+
+    Text_degE = Font_RT.render(str(round(degE)), True, black)
+    Text_degE_Rect = Text_degE.get_rect(center=(708, 255))
+
+    Text_minE = Font_RT.render(str(round(minE)), True, black)
+    Text_minE_Rect = Text_minE.get_rect(center=(725, 255))
+
+    Text_vteE = Font_RT.render(str(round(vteE, 1)), True, black)
+    Text_vteE_Rect = Text_vteE.get_rect(center=(744, 255))
+
     
     # SHOW TEXTS AND DIALS
-    screen.blits(blit_sequence=((Text_Press, Text_Press_Rect),
+    screen.blits(blit_sequence=((Text_Name, Text_Name_Rect),
+                                (Text_BatteryGS, Text_BatteryGS_Rect),
+                                (Text_Press, Text_Press_Rect),
                                 (Text_Temp, Text_Temp_Rect),
                                 (Text_O2, Text_O2_Rect),
                                 (Text_CO2, Text_CO2_Rect),
+                                (Text_Range, Text_Range_Rect),
                                 (Text_Height, Text_Height_Rect),
                                 (Text_Speed, Text_Speed_Rect),
                                 (Text_LandIn, Text_LandIn_Rect),
@@ -430,7 +503,16 @@ def phase1():
                                 (Text_Battery, Text_Battery_Rect),
                                 (Text_Sent, Text_Sent_Rect),
                                 (Text_Recieve, Text_Recieve_Rect),
-                                (Text_Humidity, Text_Humidity_Rect)))
+                                (Text_Roll, Text_Roll_Rect),
+                                (Text_Pitch, Text_Pitch_Rect),
+                                (Text_Yaw, Text_Yaw_Rect),
+                                (Text_Humidity, Text_Humidity_Rect),
+                                (Text_degN, Text_degN_Rect),
+                                (Text_minN, Text_minN_Rect),
+                                (Text_vteN, Text_vteN_Rect),
+                                (Text_degE, Text_degE_Rect),
+                                (Text_minE, Text_minE_Rect),
+                                (Text_vteE, Text_vteE_Rect)))
 
     screen.blits(blit_sequence=((RucickaTemp2, RucickaTemp_Rect2),
                                 (RucickaPress2, RucickaPress_Rect2),
@@ -462,3 +544,4 @@ while True:
         phase1()
     elif (phase == 0):
         break
+
