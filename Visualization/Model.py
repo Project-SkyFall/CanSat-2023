c67518613a14d4f9a83e0086083bfd10dee57966
brzdvt <113098495+brzdvt@users.noreply.github.com>
2023-04-26 22:53:01 +0200

    update2

--- Visualization/Model.py
+++ Visualization/Model.py
@@ -48,12 +48,12 @@ class Valec:
 
         for i in range(int(len(self.points_up)/2)):
             point = self.points_up[i]
-            x, y = self.move_point2D(point[0], point[1], roll)
-            new_point = [x, y, point[2]]
+            x, z= self.move_point2D(point[0], point[2], roll)
+            new_point = [x, point[1], z]
             y, z = self.move_point2D(new_point[1], new_point[2], pitch)
             new_point = [new_point[0], y, z]
-            x, z = self.move_point2D(new_point[0], new_point[2], yaw)
-            new_point = [x, point[1], z]
+            x, y = self.move_point2D(new_point[0], new_point[1], yaw)
+            new_point = [x, y, point[2]]
             movedpoints_up[i] = (new_point[0] + self.center[0], new_point[1] + self.center[1], new_point[2])
             j = int(i + self.num_of_points/2)
             movedpoints_down[j] = (self.center[0] - new_point[0], -new_point[1] + self.center[1], -new_point[2])
